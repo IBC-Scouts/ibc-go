cffe9571fda03ed672bb95f68befd81eb9e3e55a
srdtrk <srdtrk@hotmail.com>
2024-02-06 10:55:23 +0100

    deps: installed useful directories

---
+++ rollup/tools.go
@@ -0,0 +1,8 @@
+package rollup
+
+import (
+	_ "github.com/cometbft/cometbft/abci/types"
+	_ "github.com/cosmos/cosmos-sdk/types"
+	_ "github.com/cosmos/ibc-go/v8/modules/core/types"
+	_ "github.com/ethereum-optimism/optimism/op-node/rollup"
+)
